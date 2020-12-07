a0234e8c5cd205a657799948ec21f4260656befe
Brandon Fonnemann <branjosef@gmail.com>
2020-12-07 16:15:47 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
 import manageUsers from './reducers/manageUsers'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
+const store = createStore(
+  manageUsers
 
-// add imports and code
+)
 
 
 ReactDOM.render(
-  // add imports and code
-  <App />
+  <Provider store={store}>
+    <App/>
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
